5ecf46633882323fdeb865acce141dd179fd1329
Dan Tayari <kisa@tayari.co.il>
2025-01-28 13:10:56 +0200

    Features

--- public/pages/BugDetails.jsx
+++ public/pages/BugDetails.jsx
@@ -21,15 +21,9 @@ export function BugDetails() {
         {
             bug && 
             <div>
-<<<<<<< HEAD:public/pages/BugDetails.jsx
                 <h4>{bug.title}</h4>
                 <h5>Severity: <span>{bug.severity}</span></h5>
                 <p>{bug.description}</p>
-=======
-                <h3>{bug.title}</h3>
-                <p className="severity">Severity: <span>{bug.severity}</span></p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam esse facilis vitae numquam architecto mollitia fugiat culpa minima aperiam amet sapiente, voluptate sit, in nemo ea. Expedita iure tempore explicabo?</p>
->>>>>>> d4460168a84ead7d76bfda4454486e1e4b6fad33:pages/BugDetails.jsx
             </div>
         }
         <button><Link to="/bug">Back to List</Link></button>
